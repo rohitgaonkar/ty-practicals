104c3b062bd4eb316601e21ee5d414806e1d8634
rohitgaonkar <rohitgaonkar30@gmail.com>
2019-08-26 22:16:43 +0530

    added college folder

---
+++ college/college/demo.java
@@ -0,0 +1,21 @@
+package college;
+
+public class demo {
+
+	public static void main(String[] args) {
+		//display mentoned possition of at character 
+		 String str = "welcome to javacrip portal";
+		 char ch = str.charAt(4);
+		 System.out.println(ch);
+		
+		 //string comparision
+		 String str1="today is monday";
+		 String str2="today is tuesday";
+		 System.out.println(str1.compareTo(str2));
+
+		 //string concatinate
+		 String str3=str1.concat(str2);
+		 System.out.println(str3);
+	}
+	
+}
